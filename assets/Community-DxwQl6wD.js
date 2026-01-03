e57aaec9bb69d4249b693a71794d6beb2ed6820b
Hakimuddin <hakim@synetalsolutions.com>
2026-01-03 18:22:06 +0530

    fixed build

---
+++ assets/Community-DxwQl6wD.js
@@ -0,0 +1,1 @@
+import{C as o}from"./CustomerDashboardLayout-XBDnsAF3.js";import{_ as t}from"./CommunityFeed-DtnYKTw5.js";import{c as a,o as r,w as e,j as m}from"./app-DJPj3-n2.js";import"./NotificationDropdown-DQWgBRpK.js";import"./x-BthwGwRq.js";const l={__name:"Community",setup(s){return(_,c)=>(r(),a(o,null,{default:e(()=>[m(t)]),_:1}))}};export{l as default};
