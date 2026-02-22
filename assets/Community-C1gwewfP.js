269db6686a51ba5aa42a5907c016946a90358590
Hakimuddin <hakim@synetalsolutions.com>
2026-02-22 23:56:37 +0530

    build on stripe payment

---
+++ assets/Community-C1gwewfP.js
@@ -0,0 +1,1 @@
+import{C as o}from"./CustomerDashboardLayout-BmUA8DkZ.js";import{_ as t}from"./CommunityFeed-ChOVyrAf.js";import{a,o as r,w as e,j as m}from"./app-CZY2eBH2.js";import"./NotificationDropdown-D-NEJCdD.js";import"./x-CKX1Nhx0.js";const l={__name:"Community",setup(s){return(_,c)=>(r(),a(o,null,{default:e(()=>[m(t)]),_:1}))}};export{l as default};
---
+++ assets/Community-C36G6vvE.js
@@ -0,0 +1,1 @@
+import{P as o}from"./ProviderDashboardLayout-Dq0wjYiM.js";import{_ as a}from"./CommunityFeed-ChOVyrAf.js";import{a as r,o as t,w as e,j as m}from"./app-CZY2eBH2.js";import"./NotificationDropdown-D-NEJCdD.js";import"./x-CKX1Nhx0.js";const l={__name:"Community",setup(s){return(_,c)=>(t(),r(o,null,{default:e(()=>[m(a)]),_:1}))}};export{l as default};
